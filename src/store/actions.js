39763d9003174aec7f0b12bc675d110f3397e25f
Crowdbotics <team@crowdbotics.com>
2020-10-21 19:26:24 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,14 +11,17 @@ export const plugin50_get__readFailed = (error, starter) => ({
   error,
   starter
 })
+
 export const plugin50_get_data_g1_list = () => ({
   type: types.PLUGIN50_GET_DATA_G1_LIST
 })
+
 export const plugin50_get_data_g1_listSucceeded = (response, starter) => ({
   type: types.PLUGIN50_GET_DATA_G1_LIST_SUCCEEDED,
   response,
   starter
 })
+
 export const plugin50_get_data_g1_listFailed = (error, starter) => ({
   type: types.PLUGIN50_GET_DATA_G1_LIST_FAILED,
   error,
